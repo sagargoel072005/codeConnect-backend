b53cef9ddaa5c1eeb1d8793555ed915062bec137
sagargoel072005 <goels4280@gmail.com>
2025-07-22 13:45:53 +0530

    ui improved

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,9 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "https://codeconnect.shop"],
+      origin: [
+        "http://localhost:5173", 
+        "https://codeconnect.shop"],
       methods: ["GET", "POST"],
       credentials: true,
     },
